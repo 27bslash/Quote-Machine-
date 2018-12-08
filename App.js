43c3794c0258bf11c81e28cd44c32134c371da50
Callum Kay <greenpear68@yahoo.co.ukcode - helpcode --helpcode --help>
2018-12-08 17:07:42 +0000

    shti version

--- App.js
+++ App.js
@@ -1,14 +1,67 @@
-import {axios} from axios
 
 
-const url = '"https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json"';
+
+const apiUrl = 'https://quote-api.glitch.me/pull/1';
 
 
-getQuotes = () => {
-    axios.get(apiUrl).then(res => res.data.quotes)
-    console.log('test')
-    return 
-  };
-  quoteTest = () => {
-    getQuotes()
-  }
+
+
+
+var quoteText = document.getElementById('quote-text');
+var authorText = document.getElementById('author-text');
+
+
+var currentQuote = ''
+var currentAuthor = ''
+
+const handleClick = () => {
+  getQuotes()
+  console.log('tesr')
+}
+
+function getQuotes() {
+  axios.get(apiUrl)
+    .then((res) => {
+      console.log(res)
+      displayQuote(res.data[0].body , res.data[0].author)
+      currentQuote = res.data[0].body
+      currentAuthor = res.data[0].author
+    })
+    .catch((error) => {
+      console.log('error');
+    })
+}
+
+
+
+function displayQuote(quote, author) {
+  quoteText.textContent = quote;
+  authorText.textContent = author;
+}
+function displayAuthor() {
+  
+}
+
+const getRandomQuote = () => {
+  const i = Math.floor(Math.random() * 5)
+
+  
+
+}
+
+/*const getQuote = () => {
+  let randomQuote = getRandomQuote()
+
+  currentQuote = randomQuote.quote
+  currentAuthor = randomQuote.author
+}
+*/
+
+const newQuoteButton = document.getElementById('new-quote');
+newQuoteButton.addEventListener('click', handleClick, false)
+
+const tweetQuote = document.getElementById('tweet-quote');
+tweetQuote.setAttribute('href', 'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + '"' + currentQuote + '" ' + currentAuthor);
+
+getQuotes()
+
