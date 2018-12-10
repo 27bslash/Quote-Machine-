16768aa71f97d687e0ac2e2efd9fb213ea0cb8cd
Callum Kay <greenpear68@yahoo.co.ukcode - helpcode --helpcode --help>
2018-12-10 17:28:05 +0000

    complete with easy API

--- App.js
+++ App.js
@@ -8,8 +8,10 @@ const apiUrl = 'https://quote-api.glitch.me/pull/1';
 
 var colors = ['#16a085', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c', '#9b59b6', '#FB6964', '#342224', "#472E32", "#BDBB99", "#77B1A9", "#73A857"];
 
+const tweetQuote = document.getElementById('tweet-quote');
 var quoteText = document.getElementById('quote-text');
 var authorText = document.getElementById('author-text');
+const newQuoteButton = document.getElementById('new-quote');
 
 
 
@@ -20,19 +22,27 @@ const handleClick = () => {
 
 function changeColor() {
   var color = Math.floor(Math.random() * colors.length)
-  console.log(color);
+  var btn = document.querySelector('.button')
+  var btn2 = document.querySelector('.button2')
+
+  // body styling 
   document.body.style.backgroundColor = colors[color]
   document.body.style.color = colors[color]
   document.body.style.transition = 'all 1s'
-  document.getElementsByClassName('text').body.style.color.transition = 'all 1s'
+  //btn styling
+  btn.style.backgroundColor = colors[color]
+  btn.style.transition = 'all 1s'
+  btn2.style.backgroundColor = colors[color]
+  btn2.style.transition = 'all 1s'
 
 }
 
 function getQuotes() {
   axios.get(apiUrl)
     .then((res) => {
-      console.log(res)
-      displayQuote(res.data[0].body , "-" + " " + res.data[0].author)
+      console.log('res')
+      displayQuote(res.data[0].body, "-" + " " + res.data[0].author)
+      tweetQuote.setAttribute('href', 'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + quoteText.innerHTML + '" ' + authorText.innerHTML));
     })
     .catch((error) => {
       console.log('error');
@@ -49,12 +59,9 @@ function displayQuote(quote, author) {
   authorText.textContent = author;
 }
 
-
-
-const newQuoteButton = document.getElementById('new-quote');
 newQuoteButton.addEventListener('click', handleClick, false)
 
-const tweetQuote = document.getElementById('tweet-quote');
-tweetQuote.setAttribute('href', 'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + '"' + currentQuote + '" ' + currentAuthor);
- 
-init()
+document.addEventListener('DOMContentLoaded', function () {
+  init()
+})
+
