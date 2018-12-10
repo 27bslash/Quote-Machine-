3fc85d8d3149fdd91c1d0a3af93f2a60465eccea
Callum Kay <greenpear68@yahoo.co.ukcode - helpcode --helpcode --help>
2018-12-10 17:39:38 +0000

    done with harder API

--- App.js
+++ App.js
@@ -3,7 +3,7 @@
 
 
 /// TO DO fade in on text// 
-const apiUrl = 'https://quote-api.glitch.me/pull/1';
+const apiUrl = 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json';
 
 
 var colors = ['#16a085', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c', '#9b59b6', '#FB6964', '#342224', "#472E32", "#BDBB99", "#77B1A9", "#73A857"];
@@ -36,12 +36,16 @@ function changeColor() {
   btn2.style.transition = 'all 1s'
 
 }
+function getRandom(arg) {
+  
+}
 
 function getQuotes() {
   axios.get(apiUrl)
     .then((res) => {
-      console.log('res')
-      displayQuote(res.data[0].body, "-" + " " + res.data[0].author)
+      console.log(res)
+      quoteData = res.data.quotes
+      displayQuote(quoteData[Math.floor(Math.random() * res.data.quotes.length)].quote, "-" + " " + quoteData[Math.floor(Math.random() * res.data.quotes.length)].author)
       tweetQuote.setAttribute('href', 'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + quoteText.innerHTML + '" ' + authorText.innerHTML));
     })
     .catch((error) => {
@@ -53,6 +57,7 @@ function init() {
   getQuotes();
   changeColor();
 }
+
 
 function displayQuote(quote, author) {
   quoteText.textContent = quote;
